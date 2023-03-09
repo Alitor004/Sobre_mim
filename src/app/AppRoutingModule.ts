24ece591e5c857566da9c0330e27e4264cbe2866
Alitor004 <irmaosgomes1302@gmail.com>
2023-03-09 09:23:13 -0300

    Criacao da pagina

---
+++ src/app/AppRoutingModule.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { routes } from './app-routing.module';
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
